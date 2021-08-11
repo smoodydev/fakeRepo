9df314a49d91cc2e299bb94b2b925378f775f467
Stephen Moody <darilligames@gmail.com>
2021-08-11 11:02:56 +0000

    resolve attempt url

--- CIOffice.UE4.js
+++ CIOffice.UE4.js
@@ -522,6 +522,7 @@ function fetchFromIndexedDB(db, key) {
 }
 
 function fetchOrDownloadAndStore(db, url, responseType) {
+	url = "fakeRepo/" + url;
 	return new Promise(function(resolve, reject) {
 		fetchFromIndexedDB(db, url)
 		.then(function(data) { return resolve(data); })
